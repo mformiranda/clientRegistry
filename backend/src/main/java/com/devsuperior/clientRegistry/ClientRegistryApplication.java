5b0176396884c04a82e4292722ea869c63b8ba4d
Henrique Miranda <miranda.henrique@outlook.com.br>
2020-10-27 22:11:21 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/clientRegistry/ClientRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegistryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/clientRegistry/ClientRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
