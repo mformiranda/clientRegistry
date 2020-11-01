4bd65e9a15b5dcecd452143a87dd730e9d1e9152
Henrique Miranda <miranda.henrique@outlook.com.br>
2020-11-01 16:46:01 -0300

    DTO

--- backend/src/main/java/com/devsuperior/clientRegistry/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/clientRegistry/resources/ClientResource.java
@@ -1,6 +1,5 @@
 package com.devsuperior.clientRegistry.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.clientRegistry.entities.Client;
+import com.devsuperior.clientRegistry.dto.ClientDTO;
 import com.devsuperior.clientRegistry.services.ClientService;
 
 
@@ -22,8 +21,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();		
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();		
 		
 		return ResponseEntity.ok().body(list);
 	}
